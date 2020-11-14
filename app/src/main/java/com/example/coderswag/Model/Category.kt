3278745f56ebb42fc0eac72ad69cac54ce8a50bc
cl-droid-17 <clodlar71@gmail.com>
2020-11-14 17:27:46 -0500

    lesson 04

--- app/src/main/java/com/example/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/coderswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package com.example.coderswag.Model
 
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {               // ajouté pour qu'il affiche le titre
+        return title                                // au lieu de com.example.coderswag.Model
+    }                                               // .Category@f3950c4 (memory adresses)
+}
